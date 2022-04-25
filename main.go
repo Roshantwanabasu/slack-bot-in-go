0ef873c984ba75537b3ec1878c0efd960a20916d
Roshan Twanabasu <roshan.twanabasu@worldlink.com.np>
2022-04-25 16:36:33 +0545

    Send a simple message to a channel

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	godotenv.Load(".env")
+
+	token := os.Getenv("SLACK_AUTH_TOKEN")
+	channelID := os.Getenv("SLACK_CHANNEL_ID")
+
+	//Slack Client
+	client := slack.New(token, slack.OptionDebug(true))
+
+	//Attachement to send to channel
+	attachment := slack.Attachment{
+		Pretext: "Message From Bot",
+		Text:    "Hello, I am a Slack Bot",
+		Color:   "#fff",
+		Fields: []slack.AttachmentField{
+			{
+				Title: "Date",
+				Value: time.Now().String(),
+			},
+		},
+	}
+
+	_, timestamp, err := client.PostMessage(
+		channelID,
+		slack.MsgOptionText("Bot Sent a message", false),
+		slack.MsgOptionAttachments(attachment),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Sprintf("Message was sent at" + timestamp)
+}
